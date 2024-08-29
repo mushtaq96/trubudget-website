3d7403bc452d9647d4b7f381a08fac42afdce599
mushtaq96 <mushtaq96smb@gmail.com>
2024-08-29 23:59:21 +0200

    refactor: some files

---
+++ src/components/CommunitySection.js
@@ -0,0 +1,24 @@
+// src/pages/components/CommunitySection.js
+import React from 'react';
+
+const CommunitySection = () => {
+    return (
+        <section id="community">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-12 text-center">
+                        <h2 className="section-heading">Our Community</h2>
+                        <br />
+                    </div>
+                </div>
+            </div>
+            <div className="container">
+                <div className="row">
+                    {/* Add community member components here */}
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default CommunitySection;
