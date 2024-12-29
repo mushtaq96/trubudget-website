4cc132e012fded2fe9175d2ba6c13b01d8366ae6
mushtaq96 <mushtaq96smb@gmail.com>
2024-12-30 00:24:43 +0100

    add: sm hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,61 +1,54 @@
 import React from 'react';
 import Background from '../../static/img/hero-section/Trubudget-Blured-Gradient.png';
-import LeftScreenshot from '../../static/img/hero-section/Notifications.png';
-import RightScreenshot from '../../static/img/hero-section/Workflow.png';
+import Notifications from '../../static/img/hero-section/Notifications.png';
+import Workflow from '../../static/img/hero-section/Workflow.png';
 import PrimarySchool from '../../static/img/hero-section/PrimarySchool.png';
 import TaskStatus from '../../static/img/hero-section/TaskStatus.png';
 import BudgetDistribution from '../../static/img/hero-section/BudgetDistribution.png';
-
+import TopBar from '../components/common/TopBar';
 import { ChevronDown } from 'lucide-react';
+import CustomNavbar from '../theme/Navbar';
+import ArrowDownward from '../../static/img/hero-section/ArrowDownward.png';
+import PrimarySchoolStatus from '../../static/img/hero-section/PrimarySchoolStatus.png';
 
 const Hero = () => {
     return (
-        <section className="">
-            <div className="">
-                <img src={Background} alt="Background" className="" />
+        <section className="hero-section">
 
-            </div>
+            <img src={Background} alt="Background" className="hero-background" />
 
-            <div className='leftScreenshot'>
-                <img src={LeftScreenshot} alt="Feature 1" className="" />
-            </div>
+            <div className='hero-container'>
+                <img src={PrimarySchool} alt="Primary School" className="hero-image primarySchool" />
 
-            <div className='primarySchool'>
-                <img src={PrimarySchool} alt="primary school" />
-            </div>
-
-            <div className='taskStatus'>
-                <img src={TaskStatus} alt="task status" />
-            </div>
-
-            <div className='budgetDistribution'>
-                <img src={BudgetDistribution} alt="budget distribution" />
-            </div>
-
-            <div className='rightScreenshot'>
-                <img src={RightScreenshot} alt="Feature 2" className="" />
-            </div>
-
-            <div className="">
-                <div className="hero">
-                    <h1 className="">
+                <div className='workflow-container'>
+                    <TopBar />
+                    <img src={Workflow} alt="Workflow" className="hero-image workflow" />
+                </div>
+                <div className="hero-content-container">
+                    <h1 className="hero-title">
                         <span>Financing &amp;</span>
                         <br />
                         <span>managing made</span>
                         <br />
                         <span>smarter</span>
                     </h1>
-                    <p className="mb-8">
-                        Explore our app with spend limits, tracking, approval flows, and much more - boasting a huge success rate.
+                    <p className="hero-description">
+                        Explore our app with spend limits, tracking, approval flows, and much more – boasting a huge success rate.
                     </p>
-                    <button className="duration-300">
-                        Tell me more
+                    <button className="hero-button">
+                        Tell me more <img src={ArrowDownward} alt="downward-icon" className="downward-icon" />
                     </button>
                 </div>
+                <div className='notifications-container'>
+                    <TopBar />
+                    <img src={Notifications} alt="Notifications" className="hero-image notifications" />
+                </div>
+
+                <img src={PrimarySchoolStatus} alt="Primary School Status" className="hero-image primarySchoolStatus" />
+                <img src={BudgetDistribution} alt="Budget Distribution" className="hero-image budgetDistribution" />
             </div>
 
-
-        </section>
+        </section >
     );
 };
 
